8cd3591109daf349007bd24bd4801dc0d3cb1745
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2019-10-13 18:48:47 +0200

    add cors and json format

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/hello/', (req, res) => res.send('Hello API from node Express!'));
+app.get('/hello/', (req, res) => {
+    res.set('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.set('Access-Control-Allow-Headers', 'Authorization,Content-Type');
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.set('Access-Control-Allow-Origin', '*');
+
+    const json = {message: 'Hello API from node Express!'};
+
+    return res.json(json);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
